c9b8b0ac0ece1a39441ea0d368dd1bef113e6623
melonaster3 <melonaster@gmail.com>
2022-06-27 17:51:00 -0600

    Finished Exercise 4

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -6,3 +6,9 @@ puts "Exercise 3"
 puts "----------"
 
 # Your code goes here ...
+
+@store3 = Store.find(3)
+Store.delete(3)
+
+puts Store.count 
+
--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,19 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+
+Store.create(name: 'Surrey', annual_revenue: "224000",womens_apparel: true)
+Store.create(name: 'Whistler', annual_revenue: "1900000", mens_apparel: true)
+Store.create(name: 'Yaletown', annual_revenue: "430000", mens_apparel: true, womens_apparel: true)
+
+
+@mens_stores = Store.where :mens_apparel => true, :womens_apparel => false
+
+for stores in @mens_stores 
+    puts stores.name, stores.annual_revenue
+end 
+
+
+@loadStores = Store.where "annual_revenue < ? AND womens_apparel =?", 1000000, true
+puts @loadStores.name
+
